8a272ba2418f3567d747c293b3477861ed3bdf26
Mark Batho <mark747488@gmail.com>
2022-11-26 17:16:06 +0100

    Implement add book functionality

--- app.js
+++ app.js
@@ -36,20 +36,60 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+function resetBooks(container) {
+  container.innerHTML = null;
+}
+
 function renderBooks(container) {
   myLibrary.forEach(book => {
     const row = document.createElement('tr');
-
-    row.innerHTML = `<tr data-title=${book.title}>
+    row.dataset.title = book.title;
+    row.innerHTML = `
       <td>${book.title}</td>
       <td>${book.author}</td>
       <td>${book.pages}</td>
       <td>${book.isRead ? 'Already read' : 'Not read yet'}</td>
-    </tr>`;
-
+    `;
     container.appendChild(row);
   });
 }
 
-const table = document.querySelector('table');
-renderBooks(table);
+// const table = document.querySelector('table');
+const tableBody = document.querySelector('tbody');
+renderBooks(tableBody);
+
+const overlay = document.querySelector('#overlay');
+const modal = document.querySelector('#modal');
+const openModalBtn = document.querySelector('#open-modal');
+const closeModalBtn = document.querySelector('#close-modal');
+const form = document.querySelector('form');
+
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const readStatus = document.querySelector('#status');
+
+openModalBtn.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal);
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const book = new Book(title.value, author.value, pages.value, readStatus.checked);
+  addBookToLibrary(book);
+  resetBooks(tableBody);
+  renderBooks(tableBody);
+});
+
+function openModal() {
+  overlay.classList.add('show');
+  modal.classList.add('show');
+}
+
+function closeModal() {
+  overlay.classList.remove('show');
+  modal.classList.remove('show');
+}
+
+// Add new book
+// Add remove button
+// Add 'read' toggle
